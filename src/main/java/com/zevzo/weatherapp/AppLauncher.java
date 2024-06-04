28cf53c5b33318929cd65eb2cae81521d2a53b63
Kevin Åhlin <kevin.ahlin@hotmail.com>
2024-06-04 12:11:31 +0200

    Initial commit

---
+++ src/main/java/com/zevzo/weatherapp/AppLauncher.java
@@ -0,0 +1,16 @@
+package com.zevzo.weatherapp;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // display our weather app gui
+                new WeatherAppGui().setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/zevzo/weatherapp/WeatherAppGui.java
@@ -0,0 +1,113 @@
+package com.zevzo.weatherapp;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui() {
+        // set up our gui and add a title
+        super("Weather App");
+
+        // configure gui to end the program's process once it has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // set the size of our gui (in pixels)
+        setSize(450, 650);
+
+        // load our gui at the center of the screen
+        setLocationRelativeTo(null);
+
+        // make our layout manager null to manually position our components within the gui
+        setLayout(null);
+
+        // prevent any resize of our gui
+        setResizable(false);
+
+        // add gui components
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // search field
+        JTextField searchTextField = new JTextField();
+
+        // set the location and size of your component
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        // change the font style and size
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        add(searchTextField);
+
+        // search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        // change the cursor ot a hand cursor when hovering over this button
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        // weather image
+        JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        // temperature text
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+
+        // center the text
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        // weather condition description
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0, 405, 450, 36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
+        // humidity image
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+        add(humidityImage);
+
+        // humidity text
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityText);
+
+        // windSpeed image
+        JLabel windSpeedImage = new JLabel(loadImage("src/assets/windspeed.png"));
+        windSpeedImage.setBounds(220, 500, 74, 66);
+        add(windSpeedImage);
+
+        // windSpeed text
+        JLabel windSpeedText = new JLabel("<html><b>Windspeed</b> 15km/h%</html>");
+        windSpeedText.setBounds(310, 500, 85, 55);
+        windSpeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(windSpeedText);
+    }
+
+    // used to create images in our gui components
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+            // read the image file from the path given
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            // returns an image icon so that our component can render it
+            return new ImageIcon(image);
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+
+        System.out.println("Could not find resource");
+        return null;
+    }
+
+}

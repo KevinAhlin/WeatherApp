b53ba3b4e209195878eeb2c7b2d448b716c0b114
Kevin Åhlin <kevin.ahlin@hotmail.com>
2024-06-06 13:48:50 +0200

    Add error handling for the API connection

--- src/main/java/com/zevzo/weatherapp/WeatherApp.java
+++ src/main/java/com/zevzo/weatherapp/WeatherApp.java
@@ -2,10 +2,12 @@ package com.zevzo.weatherapp;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 // retrieve weather data from API - this backend logic will fetch the latest weather data
 // from the external API and return it. The GUI will display this data to the user
@@ -29,6 +31,33 @@ public class WeatherApp {
         try {
             // call api and get a response
             HttpURLConnection connection = fetchApiResponse(urlString);
+
+            // check response status
+            if (connection.getResponseCode() != 200) {
+                System.out.println("Error: Could not connect to API");
+                return null;
+            } else {
+                // store the API results
+                StringBuilder resultJson = new StringBuilder();
+                Scanner scanner = new Scanner(connection.getInputStream());
+
+                // read and store the resulting json data into out string builder
+                while (scanner.hasNext()) {
+                    resultJson.append(scanner.nextLine());
+                }
+
+                // close scanner
+                scanner.close();
+
+                // close url connection
+                connection.disconnect();
+
+                // parse the JSON string into a JSON object
+                JSONParser parser = new JSONParser();
+                JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+                // get the list of location data the API generated from the location name
+            }
 
         } catch (Exception error) {
             error.printStackTrace();
